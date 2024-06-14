0b3630d213d6ad551225eaf330235d56a056d509
LandaAbhishek <abhishek.nandhan711@gmail.com>
2024-06-14 20:14:44 +0530

    Update faces.py

--- Problem Set 0/faces/faces.py
+++ Problem Set 0/faces/faces.py
@@ -1,1 +1,12 @@
+# define function to convert text
+def main():
+    result = convert(input())
+    print(result)
+
+# define function to replace symbols with emoji
+def convert(text):
+    text = text.replace(":)", '🙂').replace(":(", '🙁')
+    return text
+
+main()
 
