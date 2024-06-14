b3dc60ee9c7d80002ae007fb7e7fdb244844c40c
LandaAbhishek <abhishek.nandhan711@gmail.com>
2024-06-14 20:22:43 +0530

    Add files via upload

--- Problem Set 0/indoor/indoor.py
+++ Problem Set 0/indoor/indoor.py
@@ -1,1 +1,8 @@
+# defining the text variable and input function with space after the text
+text = input("text: ")
 
+# defining the lower_text variable and lower function
+lower_text = text.lower()
+
+# printing desired output
+print (lower_text)
