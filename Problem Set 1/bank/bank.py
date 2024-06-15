0bb2a09846157f5544714dabe1653e2efa247186
LandaAbhishek <abhishek.nandhan711@gmail.com>
2024-06-15 17:44:53 +0530

    Add files via upload

--- Problem Set 1/bank/bank.py
+++ Problem Set 1/bank/bank.py
@@ -1,1 +1,10 @@
+# input greatings
+greating = input("Greatings: ").lower().strip()
 
+# outcome for greatings
+if greating.startswith("hello"):
+    print("$0")
+elif greating.startswith("h"):
+    print("$20")
+else:
+    print("$100")
