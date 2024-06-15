7ab121d672d5a13fd340456b506e909ebce61ae1
LandaAbhishek <abhishek.nandhan711@gmail.com>
2024-06-15 18:32:50 +0530

    Add files via upload

--- Problem Set 4/adieu/adieu.py
+++ Problem Set 4/adieu/adieu.py
@@ -1,1 +1,14 @@
+# importing libraries
+import inflect
+p = inflect.engine()
+names_list = []
 
+# input name
+while True:
+    try:
+        name = input("Name: ").title().strip()
+        names_list.append(name)
+    except EOFError:
+        print()
+        print("Adieu, adieu, to", p.join(names_list))
+        break
